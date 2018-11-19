79a9ae207278c61d7305e0afe22fc051896625b4
Vivek Dinesh <vivek.dinesh@cubettech.com>
2018-11-19 19:25:57 +0530

    Publish all messages

--- imports/api/messages/server/publications.js
+++ imports/api/messages/server/publications.js
@@ -1,6 +1,6 @@
 import { Messages } from "../messages.js";
 
 Meteor.publish("messages", function messagesPublication() {
-  // Publish messages owned by the current user only
-  return Messages.find({ owner: this.userId });
+  // Publish all messages
+  return Messages.find();
 });
--- imports/ui/message-board/message-board-item/message-board-item.js
+++ imports/ui/message-board/message-board-item/message-board-item.js
@@ -2,7 +2,13 @@ import { Template } from "meteor/templating";
 
 import "./message-board-item.html";
 
-Template.messageBoard.events({
+Template.messageBoardItem.helpers({
+  isOwner() {
+    return this.owner === Meteor.userId();
+  }
+});
+
+Template.messageBoardItem.events({
   "click .delete-btn"() {
     Meteor.call("messages.remove", this._id);
   }
